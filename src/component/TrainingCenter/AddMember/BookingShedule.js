c56e824cf24bfc49d39284623d8b9c892375283c
divya <divyamr4865@gmail.com>
2021-04-06 19:39:47 -1200

    commit

--- src/component/TrainingCenter/AddMember/BookingShedule.js
+++ src/component/TrainingCenter/AddMember/BookingShedule.js
@@ -8,7 +8,10 @@ export default function BookingShedule(){
         <div className="booking_shedule_tra">
             <Grid container>
                 <Grid item xs={12} md={5}>
-                   <Calendar/>
+                   <Calendar
+                   heading="Booking"
+                   SelectDate="enable"
+                   />
                 </Grid>
                 <Grid item xs={12} md={7} className="tra_booing_parent">
                 <div className="tra_booking_confirm">
