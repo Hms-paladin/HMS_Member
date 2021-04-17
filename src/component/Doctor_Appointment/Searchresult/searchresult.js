b8fd7ff8b06db980810b0311d965bb67e038eac6
Edwin_Gabriel <medwin@paladinsoftwares.com>
2021-04-17 09:17:00 +0530

    category changes

--- src/component/Doctor_Appointment/Searchresult/searchresult.js
+++ src/component/Doctor_Appointment/Searchresult/searchresult.js
@@ -12,101 +12,139 @@ import Physotheropy from "../../../images/physotheropy.png";
 import BookRoom from "../../../images/bookroom.png";
 import {NavLink} from 'react-router-dom'
 import "./searchresult.scss";
-// import { Swiper, SwiperSlide } from 'swiper-react';
+
 import Swiper from 'react-id-swiper';
 import { Pagination, Navigation, Lazy } from "swiper/dist/js/swiper.esm";
-// import { Navigation } from 'swiper/dist/js/swiper.esm';
-// import 'swiper/swiper.scss';
+
+
+// const images = [
+//                     {img:Doctor,title:"Dentistry"},
+//                     {img:Nurse,title:"Hematologist"},
+//                     {img:Report,title:"Anesthesiologist"},
+//                     {img:Trainer,title:"Dermatologist"},
+//                     {img:TrainingCenter,title:"Endocrinologist"},
+//                     {img:DietMeal,title:"Family physician"},
+//                     {img:Pharmacy,title:"Gastroenterologist"},
+//                     {img:Lab,title:"Allergists/Immunologist"},
+//                     {img:Physotheropy,title:"Radiologist"},
+//                     {img:BookRoom,title:"Neurologist"},
+//                     {img:TrainingCenter,title:"ENT"},
+//                     {img:Doctor,title:"Cardiologist"},
+//                     {img:Lab,title:"Allergists/Immunologist"},
+//                     {img:Physotheropy,title:"Radiologist"},
+//                     {img:BookRoom,title:"Neurologist"},
+//                     {img:TrainingCenter,title:"ENT"},
+//                     {img:Doctor,title:"Cardiologist"}
+// ]
+
+// const styles={
+// 	imgContainer:{
+// 			    width:'17%',
+//                 height:'auto',
+//                 margin:'20px 1.5%',
+//                 borderRadius: '2px',
+//                 boxshadow: '0px 5px 5px 2px #ded5de',
+// 	},
+// 	container:{
+// 		borderBottom:'0.3px solid #c8c6c6'
+// 	},
+// }
+// const params = {
+//     modules: [ Navigation,Lazy],
+//     slidesPerView:'5',
+//       slidesPerColumn: '2',
+//       direction: 'horizontal',
+//     //   preloadImages: false,
+//       lazy: true,
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         // prevEl: ".swiper-button-prev",
+//     },
+//     pagination:{
+//         el:'.swiper-pagination',
+//         clickable:true,
+//       },
+//     loop: false,
+//     spaceBetween: 30,
+//     //   pagination:{
+//     //     el:'.swiper-pagination',
+//     //     clickable:true,
+//     //   },
+//     };
+ 
+// function Searchresult(props) {
+//     const [swiper,setswiper]=useState(null)
+//     const goNext = () => {
+//         if (swiper !== null) {
+//           swiper.slideNext();
+//         }
+//       };
+     
+//     return(
+//         <>
+//         <div className="search_spl_parent">
+//           <div className="container"> 
+//             <div>
+//         <Swiper {...params} getSwiper={images}>
+     
+//      {images.map((data)=>{
+//             return(
+//             <div className="imgContainer" style={styles.imgContainer}>
+//                <NavLink to="/feed"><img src = {data.img}/></NavLink> 
+//                 <div className="imgTitle">{data.title}</div>
+//         </div> 
+       
+//             )  
+//         })}
+       
+     
+//          </Swiper>
+//          </div>
+//          </div>
+//         <div><ArrowForwardIosIcon className="tra_right_arrow" onClick={goNext}/></div>
+        
+//         </div>
+//         </>
+//     )
+// }
+
+
+
 
 const images = [
-                    {img:Doctor,title:"Dentistry"},
-                    {img:Nurse,title:"Hematologist"},
-                    {img:Report,title:"Anesthesiologist"},
-                    {img:Trainer,title:"Dermatologist"},
-                    {img:TrainingCenter,title:"Endocrinologist"},
-                    {img:DietMeal,title:"Family physician"},
-                    {img:Pharmacy,title:"Gastroenterologist"},
-                    {img:Lab,title:"Allergists/Immunologist"},
-                    {img:Physotheropy,title:"Radiologist"},
-                    {img:BookRoom,title:"Neurologist"},
-                    {img:TrainingCenter,title:"ENT"},
-                    {img:Doctor,title:"Cardiologist"},
-                    {img:Lab,title:"Allergists/Immunologist"},
-                    {img:Physotheropy,title:"Radiologist"},
-                    {img:BookRoom,title:"Neurologist"},
-                    {img:TrainingCenter,title:"ENT"},
-                    {img:Doctor,title:"Cardiologist"}
+  {img:Doctor,title:"Dentistry"},
+  {img:Nurse,title:"Hematologist"},
+  {img:Report,title:"Anesthesiologist"},
+  {img:Trainer,title:"Dermatologist"},
+  {img:TrainingCenter,title:"Endocrinologist"},
+  {img:DietMeal,title:"Family physician"},
+  {img:Pharmacy,title:"Gastroenterologist"},
+  {img:Lab,title:"Allergists/Immunologist"},
+  {img:Physotheropy,title:"Radiologist"},
+  {img:BookRoom,title:"Neurologist"},
+  {img:TrainingCenter,title:"ENT"},
+  {img:Doctor,title:"Cardiologist"}
 ]
 
-const styles={
-	imgContainer:{
-			    width:'17%',
-                height:'auto',
-                margin:'20px 1.5%',
-                borderRadius: '2px',
-                boxshadow: '0px 5px 5px 2px #ded5de',
-	},
-	container:{
-		borderBottom:'0.3px solid #c8c6c6'
-	},
-}
-const params = {
-    modules: [ Navigation,Lazy],
-    slidesPerView:'5',
-      slidesPerColumn: '2',
-      direction: 'horizontal',
-    //   preloadImages: false,
-      lazy: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        // prevEl: ".swiper-button-prev",
-    },
-    pagination:{
-        el:'.swiper-pagination',
-        clickable:true,
-      },
-    loop: false,
-    spaceBetween: 30,
-    //   pagination:{
-    //     el:'.swiper-pagination',
-    //     clickable:true,
-    //   },
-    };
- 
-function Searchresult(props) {
-    const [swiper,setswiper]=useState(null)
-    const goNext = () => {
-        if (swiper !== null) {
-          swiper.slideNext();
-        }
-      };
-     
-    return(
-        <>
-        <div className="search_spl_parent">
-          <div className="container"> 
-            <div>
-        <Swiper {...params} getSwiper={images}>
-     
-     {images.map((data)=>{
-            return(
-            <div className="imgContainer" style={styles.imgContainer}>
-               <NavLink to="/feed"><img src = {data.img}/></NavLink> 
-                <div className="imgTitle">{data.title}</div>
-        </div> 
-       
-            )  
-        })}
-       
-     
-         </Swiper>
-         </div>
-         </div>
-        <div><ArrowForwardIosIcon className="tra_right_arrow" onClick={goNext}/></div>
-        
-        </div>
-        </>
-    )
-}
 
+
+function Searchresult(props) {
+return(
+<>
+<div className="search_spl_parent">
+<div className="container">
+{images.map((data)=>{
+return(
+<div className="imgContainer">
+<NavLink to="/feed"><img src = {data.img} /></NavLink> 
+<div className="imgTitle">{data.title}</div>
+</div> 
+)  
+})}
+</div>
+<div><ArrowForwardIosIcon className="tra_right_arrow"/></div>
+</div>
+</>
+)
+}
 export default Searchresult;
